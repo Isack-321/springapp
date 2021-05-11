6b0f9d0366d74099428f749a2edd6295894a4d15
Isack <ituwei21@gmail.com>
2021-05-11 20:46:24 +0300

    fixed some errors

--- src/main/java/com/example/demo/appuser/userRepo.java
+++ src/main/java/com/example/demo/appuser/userRepo.java
@@ -1,12 +1,14 @@
 package com.example.demo.appuser;
 
 import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 @Transactional(readOnly = true)
-public interface userRepo {
+public interface userRepo extends JpaRepository<Appuser, Long> {
 
 	Optional<Appuser> findByEmail(String email);
 		
--- src/main/java/com/example/demo/registration/registrationService.java
+++ src/main/java/com/example/demo/registration/registrationService.java
@@ -1,7 +1,9 @@
 package com.example.demo.registration;
 
 import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties.Registration;
+import org.springframework.stereotype.Service;
 
+@Service
 public class registrationService {
 
 	public static String regRequest(Registration request) {
